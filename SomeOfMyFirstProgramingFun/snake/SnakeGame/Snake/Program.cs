ea11a85ae38eca3f5d48cc7cd2658fe579c506ad
valsinev <streamline_@abv.bg>
2017-04-10 23:45:21 +0300

    added food, moving, eating the food and growing logic

--- SomeOfMyFirstProgramingFun/snake/SnakeGame/Snake/Program.cs
+++ SomeOfMyFirstProgramingFun/snake/SnakeGame/Snake/Program.cs
@@ -2,65 +2,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
 
 namespace Snake
 {
     class Program
     {
+        private static byte direction;
+        private static byte down = 0;
+        private static byte up = 1;
+        private static byte left = 2;
+        private static byte right = 3;
+        private static int speedIncreaser = 4;
+        private static int gameSpeed = 200;
+
         static void Main(string[] args)
         {
-            var nextPosition = new Position(0, 0);
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
 
-            Queue<Position> body = new Queue<Position>();
+            Queue<Position> snakeBody = new Queue<Position>();
+
+            Position[] directions =
+            {
+            new Position(0, 1),
+            new Position(0, -1),
+            new Position(-1, 0),
+            new Position(1, 0)
+            };
 
             for (int i = 0; i < 5; i++)
             {
-                body.Enqueue(new Position(i, 0));
+                snakeBody.Enqueue(new Position(i, 0));
             }
 
-            foreach (Position position in body)
+            Position nextPosition;
+
+            foreach (Position position in snakeBody)
             {
                 nextPosition = position;
                 Console.SetCursorPosition(nextPosition.Col, nextPosition.Row);
-                Console.Write("o");
+                Console.Write("*");
             }
+
+            var randomNumberGenerator = new Random();
+
+            var food = new Position(
+                randomNumberGenerator.Next(0, Console.WindowWidth),
+                randomNumberGenerator.Next(0, Console.WindowHeight));
+            Console.SetCursorPosition(food.Col, food.Row);
+            Console.Write("Q");
 
             while (true)
             {
-                //moving the snake
-                var input = Console.ReadKey().Key;
-                nextPosition = new Position(
-                    nextPosition.Col + Direction(input).Col,
-                    nextPosition.Row + Direction(input).Row);
+                if (Console.KeyAvailable)
+                {
+                    //moving the snake
+                    var input = Console.ReadKey().Key;
 
+                    if (input == ConsoleKey.RightArrow)
+                    {
+                        if (direction != left) direction = right;
+                    }
+                    if (input == ConsoleKey.LeftArrow)
+                    {
+                        if (direction != right) direction = left;
+                    }
+                    if (input == ConsoleKey.UpArrow)
+                    {
+                        if (direction != down) direction = up;
+                    }
+                    if (input == ConsoleKey.DownArrow)
+                    {
+                        if (direction != up) direction = down;
+                    }
+                }
 
+                var currentHead = snakeBody.Last();
+                var newHead = new Position(
+                    currentHead.Col + directions[direction].Col,
+                    currentHead.Row + directions[direction].Row);
+                snakeBody.Enqueue(newHead);
 
-                Console.SetCursorPosition(nextPosition.Col, nextPosition.Row);
-                Console.Write("o");
-            }
-        }
-        public static Position Direction(ConsoleKey key)
-        {
-            switch (key)
-            {
-                case ConsoleKey.DownArrow:
+                Console.SetCursorPosition(newHead.Col, newHead.Row);
+                Console.Write("#");
+
+                //eating the food
+                if (snakeBody.Contains(food))
+                {
+                    food = new Position(
+                        randomNumberGenerator.Next(0, Console.WindowWidth),
+                        randomNumberGenerator.Next(0, Console.WindowHeight));
+
+                    Console.SetCursorPosition(food.Col, food.Row);
+                    Console.Write("Q");
+
+                    if (gameSpeed - speedIncreaser < 0)
                     {
-                        return new Position(0, 1);
+                        gameSpeed = 0;
                     }
-                case ConsoleKey.UpArrow:
+                    else
                     {
-                        return new Position(0, -1);
+                        gameSpeed -= speedIncreaser;
                     }
-                case ConsoleKey.LeftArrow:
-                    {
-                        return new Position(-1, 0);
-                    }
-                case ConsoleKey.RightArrow:
-                    {
-                        return new Position(1, 0);
-                    }
-                default:
-                    throw new ArgumentException("No sutch direction");
+                }
+                else
+                {
+                    var lastElement = snakeBody.Dequeue();
+                    Console.SetCursorPosition(lastElement.Col, lastElement.Row);
+                    Console.Write(" ");
+                }
+
+                Thread.Sleep(gameSpeed);
             }
         }
     }
