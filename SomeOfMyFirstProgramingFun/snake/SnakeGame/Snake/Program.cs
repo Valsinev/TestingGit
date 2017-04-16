ac83cb693e68c55cabb9683b306cb1d42ca9770b
Valsinev <streamline_@abv.bg>
2017-04-16 19:14:36 +0300

    added colors and some code refactoring

--- SomeOfMyFirstProgramingFun/snake/SnakeGame/Snake/Program.cs
+++ SomeOfMyFirstProgramingFun/snake/SnakeGame/Snake/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Linq;
-
-namespace Snake
+﻿namespace Snake
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+
     public class Program
     {
-        private static byte direction;
+        private static byte direction = 0;
         private static byte right = 0;
         private static byte left = 1;
         private static byte down = 2;
@@ -17,8 +17,11 @@ namespace Snake
         private static char snakeBodyElement = '0';
         private static char snakeFoodElement = 'Q';
         private static long gameScore = 0;
+        private static ConsoleColor appleColor = ConsoleColor.Red;
+        private static ConsoleColor endGameColor = ConsoleColor.Yellow;
+        private static ConsoleColor snakeColor = ConsoleColor.Green;
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.BufferHeight = Console.WindowHeight;
             Console.BufferWidth = Console.WindowWidth;
@@ -28,9 +31,9 @@ namespace Snake
             Position[] directions =
             {
             new Position(1, 0), // right
-            new Position(-1, 0), //left
-            new Position(0, 1), //down
-            new Position(0, -1), //up
+            new Position(-1, 0), // left
+            new Position(0, 1), // down
+            new Position(0, -1), // up
             };
 
             for (int i = 0; i < 5; i++)
@@ -40,8 +43,8 @@ namespace Snake
 
             foreach (Position position in snakeBody)
             {
-                Console.SetCursorPosition(position.Col, position.Row);
-                Console.Write(snakeBodyElement);
+                Console.ForegroundColor = snakeColor;
+                DrawingManager(position, snakeBodyElement);
             }
 
             var randomNumberGenerator = new Random();
@@ -49,8 +52,9 @@ namespace Snake
             var food = new Position(
                 randomNumberGenerator.Next(0, Console.WindowWidth),
                 randomNumberGenerator.Next(0, Console.WindowHeight));
-            Console.SetCursorPosition(food.Col, food.Row);
-            Console.Write(snakeFoodElement);
+
+            Console.ForegroundColor = appleColor;
+            DrawingManager(food, snakeFoodElement);
 
             while (true)
             {
@@ -58,22 +62,7 @@ namespace Snake
                 {
                     var input = Console.ReadKey().Key;
 
-                    if (input == ConsoleKey.RightArrow)
-                    {
-                        if (direction != left) direction = right;
-                    }
-                    if (input == ConsoleKey.LeftArrow)
-                    {
-                        if (direction != right) direction = left;
-                    }
-                    if (input == ConsoleKey.UpArrow)
-                    {
-                        if (direction != down) direction = up;
-                    }
-                    if (input == ConsoleKey.DownArrow)
-                    {
-                        if (direction != up) direction = down;
-                    }
+                    direction = DirectionSelecter(input, direction);
                 }
 
                 var currentHead = snakeBody.Last();
@@ -81,15 +70,31 @@ namespace Snake
                     currentHead.Col + directions[direction].Col,
                     currentHead.Row + directions[direction].Row);
 
-                if (newHead.Col < 0) newHead.Col = Console.WindowWidth - 1;
-                if (newHead.Col >= Console.WindowWidth) newHead.Col = 0;
-                if (newHead.Row < 0) newHead.Row = Console.WindowHeight - 1;
-                if (newHead.Row >= Console.WindowHeight) newHead.Row = 0;
+                if (newHead.Col < 0)
+                {
+                    newHead.Col = Console.WindowWidth - 1;
+                }
 
-                //ending the gameafther collision
+                if (newHead.Col >= Console.WindowWidth)
+                {
+                    newHead.Col = 0;
+                }
+
+                if (newHead.Row < 0)
+                {
+                    newHead.Row = Console.WindowHeight - 1;
+                }
+
+                if (newHead.Row >= Console.WindowHeight)
+                {
+                    newHead.Row = 0;
+                }
+
+                // ending the game afther collision with body
                 if (snakeBody.Contains(newHead))
                 {
                     Console.SetCursorPosition(0, 0);
+                    Console.ForegroundColor = endGameColor;
                     Console.WriteLine("Game over!");
                     Console.WriteLine("Your score is {0}", gameScore);
                     Console.ReadLine();
@@ -97,18 +102,18 @@ namespace Snake
                 }
 
                 snakeBody.Enqueue(newHead);
-                Console.SetCursorPosition(newHead.Col, newHead.Row);
-                Console.Write(snakeBodyElement);
+                Console.ForegroundColor = snakeColor;
+                DrawingManager(newHead, snakeBodyElement);
 
-                //eating the food
+                // eating the food
                 if (snakeBody.Contains(food))
                 {
                     food = new Position(
                         randomNumberGenerator.Next(0, Console.WindowWidth),
                         randomNumberGenerator.Next(0, Console.WindowHeight));
 
-                    Console.SetCursorPosition(food.Col, food.Row);
-                    Console.Write(snakeFoodElement);
+                    Console.ForegroundColor = appleColor;
+                    DrawingManager(food, snakeFoodElement);
 
                     gameScore += 100;
 
@@ -124,12 +129,67 @@ namespace Snake
                 else
                 {
                     var lastElement = snakeBody.Dequeue();
-                    Console.SetCursorPosition(lastElement.Col, lastElement.Row);
-                    Console.Write(" ");
+                    DrawingManager(lastElement, ' ');
                 }
 
                 Thread.Sleep(gameSpeed);
             }
+        }
+
+        private static void DrawingManager(Position position, char writingElement)
+        {
+            Console.SetCursorPosition(position.Col, position.Row);
+            Console.Write(writingElement);
+        }
+
+        private static byte DirectionSelecter(ConsoleKey input, byte previousDirection)
+        {
+            byte result = previousDirection;
+
+            switch (input)
+            {
+                case ConsoleKey.LeftArrow:
+                    {
+                        if (previousDirection != right)
+                        {
+                            result = left;
+                        }
+
+                        break;
+                    }
+
+                case ConsoleKey.UpArrow:
+                    {
+                        if (previousDirection != down)
+                        {
+                            result = up;
+                        }
+
+                        break;
+                    }
+
+                case ConsoleKey.RightArrow:
+                    {
+                        if (previousDirection != left)
+                        {
+                            result = right;
+                        }
+
+                        break;
+                    }
+
+                case ConsoleKey.DownArrow:
+                    {
+                        if (previousDirection != up)
+                        {
+                            result = down;
+                        }
+
+                        break;
+                    }
+            }
+
+            return result;
         }
     }
 }
